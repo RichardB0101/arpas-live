b329d456e436811beadc51f3070e2099f3f99d41
SundayCoder81 <richard.bogu@gmail.com>
2022-11-12 22:17:13 +0200

    one one

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('viewOcenianiePrac');
 })->middleware(['auth'])->name('dashboard');
 
 
