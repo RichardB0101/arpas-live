f8dcf194255eaee1264f6ac49ba7ec623025e1aa
SundayCoder81 <richard.bogu@gmail.com>
2022-12-11 09:48:59 +0200

    shadow changed

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -3,12 +3,12 @@
 @section('body-class', 'd-flex flex-column h-100')
 @section('content')
     <section style="background-color: #eee;">
-        <div class="container py-5">
+        <div class="container py-5 ">
             <h1 class="my-4 text-center">Profil</h1>
 
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="card mb-4">
+            <div class="row ">
+                <div class="col-lg-4 ">
+                    <div class="card mb-4 shadow bg-body rounded">
                         <div class="card-body text-center">
                             <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
                                  alt="avatar"
@@ -22,7 +22,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="card mb-4 mb-lg-0">
+                    <div class="card mb-4 mb-lg-0 shadow bg-body rounded">
                         <div class="card-body p-0">
                             <ul class="list-group list-group-flush rounded-3">
                                 <p class="text-center mt-3">Ostatnio wysłane</p>
@@ -39,8 +39,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-8">
-                    <div class="card mb-4">
+                <div class="col-lg-8 ">
+                    <div class="card mb-4 shadow bg-body rounded">
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-sm-3">
@@ -89,8 +89,8 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-6">
-                            <div class="card mb-4 mb-md-0">
+                        <div class="col-md-6 ">
+                            <div class="card mb-4 mb-md-0 shadow bg-body rounded">
                                 <div class="card-body">
                                     <p class="mb-4"><span class="text-primary font-italic me-1">zaliczenia</span>
                                         Semestr zimowy 2022/2023
@@ -129,7 +129,7 @@
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <div class="card mb-4 mb-md-0">
+                            <div class="card mb-4 mb-md-0 shadow bg-body rounded">
                                 <div class="card-body">
                                     <p class="mb-4"><span class="text-primary font-italic me-1">zaliczenia</span>
                                         Semestr letni 2022/2023
